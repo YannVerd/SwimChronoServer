bc786c6b3c4236fe310f33fd3062bac5aa1946ce
YannV <yann.e.verdier@gmail.com>
2024-11-06 22:15:16 +0000

    delete console log

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -26,28 +26,23 @@ export const registerUser = async (req: Request, res: Response) => {
 }
 
 export const loginUser = async (req: Request, res: Response ) => {
-    console.log('try to login:', req.body)
     const user = new User();
     User.findOne({email: req.body.email})
         .then(doc => {
-            console.log('login findOne', doc)
             if(doc){
                 user.password = doc.password
                 if(user.validPassword(req.body.password)){
-                    console.log('validPassword okay')
                     const token = generateAccessToken(doc.userId)
-                    res.status(200).json({user: {username: doc.username, email: doc.email}, token: token})
+                    res.status(200).json({user: {username: doc.username, email: doc.email, id: doc._id.toString()}, token: token})
                 }else{
                     res.status(404).json({message: "Wrong password. Please retry"})
                 }
             }else{
-                console.log("no doc:", doc)
                 res.status(404).json({message: "No user was found with this email address"})
             }  
             
         })
         .catch(err => {
-            console.error(err);
             res.status(404).json({message: "Wrong email. No user found"})
         })
             
